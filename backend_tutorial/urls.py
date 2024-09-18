ae996bf6559896174ccdc11cd8044504f3ae05cc
holarora <liu.lijia.82v@st.kyoto-u.ac.jp>
2024-09-18 13:43:11 +0900

    fly dev prep

--- backend_tutorial/urls.py
+++ backend_tutorial/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("alltasks/", include("snippets.urls")),
     path("daily/", include("daily.urls")),
-    path("admin/", admin.site.urls),
+    path("iamadmin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
